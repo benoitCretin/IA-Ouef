7d21ebfbb73d0569b4adb88b4c48c093e7c25f8c
benoitCretin <benoit.cretin@iut-dijon.u-bourgogne.fr>
2024-05-04 19:35:30 +0200

    StrategyLootingStack V1 (esquive les monstres)

--- IAOuef/IA/Strategies/StrategyLootingStack.cs
+++ IAOuef/IA/Strategies/StrategyLootingStack.cs
@@ -10,6 +10,7 @@ namespace IA.Strategies
 {
     public class StrategyLootingStack : IStrategie
     {
+        private int playerNumber;
         public void Executer()
         {
             Player myPlayer = new Player();
@@ -18,15 +19,65 @@ namespace IA.Strategies
             if (message.StartsWith("Bonjour"))
             {
                 string number = message.Split("|")[1];
-                myPlayer.Number = Convert.ToInt32(number);
+                playerNumber = Convert.ToInt32(number);
             }
 
-            while ((message = Connection.RecevoirMessage()) != "FIN") ;
+            while ((message = Connection.RecevoirMessage()) != "FIN")
             {
                 if (message.StartsWith("DEBUT_TOUR"))
                 {
                     string[] number = message.Split('|');
                     game.TurnNumber = Convert.ToInt32(number[1]);
+                    game.UpdatePlayers();
+                    game.UpdateRoads();
+                    if (game.Players[playerNumber].ChestNumber == 5)
+                    {
+                        Connection.EnvoyerMessage("RECELER");
+                    }
+                    else 
+                    if (game.Players[playerNumber].Hp <= 3)
+                    {
+                        Connection.EnvoyerMessage("REPARER");
+                    }
+                    else
+                    {
+                        int nbactions = 0;
+                        bool hasMoreAttack = true;
+                        while (game.Players[playerNumber].Score >= 500 && hasMoreAttack && nbactions < 14)
+                        {
+                            hasMoreAttack = false;
+                            foreach(Player player in game.Players.Values)
+                            {
+                                if (player.Attack > game.Players[playerNumber].Attack) hasMoreAttack = true;
+                            }
+                            if (hasMoreAttack)
+                            {
+                                Connection.EnvoyerMessage("RECRUTER");
+                                nbactions++;
+                            }
+                        }
+                        int nbPillage = 0;
+                        int butin = 0;
+                        foreach (Road road in game.Roads)
+                        {
+                            if (game.Players[playerNumber].Attack >= road.AttackValue)
+                            {
+                                if (road.ChestValue1 > butin && !road.HasMonster)
+                                {
+                                    nbPillage = road.Number;
+                                    butin = road.ChestValue1;
+                                }
+                            }
+                        }
+                        if (nbPillage == 0)
+                        {
+                            Connection.EnvoyerMessage("REPARER");
+                        }
+                        else
+                        {
+                            Connection.EnvoyerMessage("PILLER|" +  Convert.ToString(nbPillage));
+                        }
+                    }
                 }
             }
 
