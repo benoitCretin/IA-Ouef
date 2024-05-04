6dea3c2f628682fe88ce28f7aed76c54af20882b
benoitCretin <benoit.cretin@iut-dijon.u-bourgogne.fr>
2024-05-04 16:30:55 +0200

    Création Strategy Looting Stack

---
+++ IAOuef/IA/Strategies/StrategyLootingStack.cs
@@ -0,0 +1,22 @@
+﻿using IA.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA.Strategies
+{
+    public class StrategyLootingStack : IStrategie
+    {
+        public void Executer()
+        {
+            Connection.RecevoirMessage();
+            string message = Connection.RecevoirMessage();
+            if (message.StartsWith("DEBUT_TOUR"))
+            {
+                string number = message.Split('|')[1];
+            }
+        }
+    }
+}
